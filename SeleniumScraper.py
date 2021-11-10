519a9ada249d31a8ac95bf0efa00a7c9d656f1ef
FaeqAlrimawi <faeq.rimawi@gmail.com>
2021-11-10 13:17:57 +0000

    added SeleniumScraper pyhthon script

--- SeleniumScraper.py
+++ SeleniumScraper.py
@@ -8,6 +8,9 @@ import requests
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 #Install Driver
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -17,12 +20,27 @@ search_url= "https://www.simple.co.uk/acts-of-kindness.html"
 
 driver.get(search_url)
 
-#Locate the images to be scraped from the current page
-imgResults = driver.find_elements(By.XPATH, "/html/body/div[1]/div/main/section[1]/div/div[3]/div/div/div/div[2]/div/div[3]/div/div/div[1]/div/div[1]/h1")
-# imgResults = driver.find_elements_by_xpath('//td[@class="name"]')
-totalResults=len(imgResults)
+#switch to internal frame
+driver.switch_to.frame(driver.find_element(By.CSS_SELECTOR, 'iframe'))
 
-print(len(imgResults))
+#Locate the elements of AoKs to be scraped from the current page
+actResults = driver.find_elements(By.XPATH, "//h1[@class='xTitle xHeading']")
+
+for element in actResults:
+    print(element.text)
+
+#move to the next page
+# btnNext = driver.find_element(By.XPATH, "//a[@class='xPagingButton xActionPaginate xActionShowNext']")
+# driver.execute_script("arguments[0].click();", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//a[@class='xPagingButton xActionPaginate xActionShowNext']"))))
+# driver.execute_script("arguments[0].click();", btnNext)
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[@class='xPagingButton xActionPaginate xActionShowNext']"))).click()
+
+actResults = driver.find_elements(By.XPATH, "//h1[@class='xTitle xHeading']")
+
+for element in actResults:
+    print(element.text)
+
+# btnNext.click()
 
 driver.quit()
 
