1c1094373d3ed392c4985ff390b7ca6282b32fde
FaeqAlrimawi <faeq.rimawi@gmail.com>
2021-11-13 17:48:40 +0000

    added SeleniumScraper pyhthon script

--- SeleniumScraper.py
+++ SeleniumScraper.py
@@ -6,6 +6,7 @@ from PIL import Image
 import io
 import requests
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import DriverManager
 from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -13,7 +14,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 #Install Driver
-driver = webdriver.Chrome(ChromeDriverManager().install())
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Firefox(executable_path="F:\PycharmProjects\geckodriver.exe")
 
 #Specify Search URL
 search_url= "https://www.simple.co.uk/acts-of-kindness.html"
@@ -30,10 +32,13 @@ for element in actResults:
     print(element.text)
 
 #move to the next page
-# btnNext = driver.find_element(By.XPATH, "//a[@class='xPagingButton xActionPaginate xActionShowNext']")
+btnNext = driver.find_element(By.XPATH, "//a[@class='xPagingButton xActionPaginate xActionShowNext']")
 # driver.execute_script("arguments[0].click();", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//a[@class='xPagingButton xActionPaginate xActionShowNext']"))))
 # driver.execute_script("arguments[0].click();", btnNext)
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[@class='xPagingButton xActionPaginate xActionShowNext']"))).click()
+# WebDriverWait(driver, 10).until(EC.element_to_be_clickable(btnNext)).click()
+WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it(driver.find_element(By.XPATH, '//*[@id="ngxFrame911cf8af-4547-4844-9588-8581559255a0"]"')))
+# time.sleep(30)
+# print(btnNext.tag_name, btnNext.text)
 
 actResults = driver.find_elements(By.XPATH, "//h1[@class='xTitle xHeading']")
 
